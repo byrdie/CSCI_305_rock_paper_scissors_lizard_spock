b741a4680909c0daae163bcf1268d3a0d3f2e139
byrdie <roytsmart@gmail.com>
2015-03-20 21:24:42 -0600

    Submit for grade

--- smart_roy.lab2.py
+++ smart_roy.lab2.py
@@ -6,6 +6,7 @@
 
 from enum import Enum
 import random
+from random import SystemRandom
 
 # Global variables for last move bot
 last_elem_1 = 0
@@ -32,39 +33,10 @@ class Play(Enum):
 	RandomBot = 3
 	IterativeBot = 4
 	LastPlayBot = 5
-	MyBot = 6
-
-# enumeration for storing the column elements
-class Col(Enum):
-	round_num = 0
-	choice_1 = 1
-	result_1 = 2
-	sep = 3
-	choice_2 = 4
-	result_2 = 5
-	msg = 6
-	
+	MyBot = 6	
 
 # Superclass for each of the elements of the game: rock, paper, scissors, lizard, spock
 class Element:
-
-	strs = [
-		"Rock equals Rock",
-		"Rock crushes Lizard",
-		"Rock crushes Scissors",
-		"Paper equals Paper",
-		"Paper covers Rock",
-		"Paper disproves Spock",	
-		"Scissors equals Scissors"
-		"Scissors cut Paper",
-		"Scissors decapitate Lizard",
-		"Spock equals Spock",
-		"Spock smashes Scissors",
-		"Spock vaporizes Rock",
-		"Lizard equals Lizard",	
-		"Lizard poisons Spock",
-		"Lizard eats Paper",
-	]
 
 	# Constructor for objects of type Element (Not sure why this is here, it would be more 
 	# efficient for each subclass to have its own constructor)
@@ -296,7 +268,7 @@ class Stupid_bot(Player):
 
 	# return the same move each time
 	def play(self):
-		return elements[Elem.rock]()
+		return Player.elements[Elem.rock]()
 
 # Subclass for random bot
 class Random_bot(Player):
@@ -317,6 +289,7 @@ class Iterative_bot(Player):
 		self.i = (self.i + 1) % 5		# 5 possible elements
 		return Player.elements[self.elem_list[self.i]]()
 
+# Subclass for Last Play Bot
 class Last_play_bot(Player):
 
 	# Play the move the opponent used last round
@@ -333,6 +306,7 @@ class Last_play_bot(Player):
 		else:
 			return Player.elements[Elem.rock]()
 
+# Subclass for Human input
 class Human(Player):
 
 	# Play the move the user picks
@@ -354,21 +328,38 @@ class Human(Player):
 
 		return Player.elements[Elem(choice - 1)]()	# offset for zero based indices
 
+# Class to store instance of custom bot
+class My_bot(Player):
 
+	# Cryptographically secure RNG
+	cryptogen = SystemRandom()
+
+	# Pit the system random number generator aginst the Python pseudo-random number generator
+	# Apologies if this does not work on your system
+	def play(self):
+
+		# Generate new random number
+		crypt = self.cryptogen.randrange(5)
+
+		return self.elements[Elem(crypt)]()
 
 
 # Not sure why we're implementing a main class. Classes should not be created for only one instance
 class Main():
 
-	# Constructor for main class
-	# def __init__(self):
+	# dictionary for possible players
+	players = {
+		Play.HumanNotBot : Human,
+		Play.StupidBot : Stupid_bot,
+		Play.RandomBot : Random_bot,
+		Play.IterativeBot : Iterative_bot,
+		Play.LastPlayBot : Last_play_bot,
+		Play.MyBot : My_bot
+	}
 
 	# Main method
 	def main():
 
-		# Array for storing the results of the matches
-		cols = 7
-		rows = 7
 		# results = [[0 for x in range(cols)] for x in range(rows)]
 		results = {}
 
@@ -385,16 +376,15 @@ class Main():
 		print("Please pick two players by entering the corresponding number") 
 		max_player = Main.print_players()
 		print()
-		print("Select Player 1") 
-		# this should be changed to scan input later
-		# player_1 = Iterative_bot(Play.IterativeBot)
-		# player_1 = Random_bot(Play.RandomBot, 1)
-		# player_1 = Last_play_bot(Play.LastPlayBot, 1)
-		player_1 = Human(Play.HumanNotBot, 1)
-		print("Select Player 2")
-		# also scan for input here
-		# player_2 = Iterative_bot(Play.IterativeBot, 2)
-		player_2 = Random_bot(Play.RandomBot, 2)
+
+		print("Select Player 1:") 
+		choice = Play(Main.get_player())
+		player_1 = Main.players[choice](choice, 1)
+
+		print("Select Player 2:")
+		choice = Play(Main.get_player())
+		player_2 = Main.players[choice](choice, 2)
+
 		print(player_1.name().name + " vs. " + player_2.name().name + " for five rounds\n")
 
 		results[0] = ["", "Player 1", player_1.name().name, "", "Player 2", player_2.name().name, ""]
@@ -450,7 +440,7 @@ class Main():
 			print("{: <10} {: <10} {: <11} {: <10} {: <10} {: <15} {: <20}".format(*results[row]))
 
 		# Print the score and the winner
-		print("The score is ", player_1_wins, " to ", player_2_wins)
+		print("The score is ", player_1_wins, "to", player_2_wins)
 
 		if player_1_wins > player_2_wins:
 			print("Player 1 won the game!")
@@ -459,32 +449,27 @@ class Main():
 		else:
 			print("The game was a draw")
 
-
-
-
-
-
 	# Prints out list of players
 	def print_players():
 		for name, member in Play.__members__.items():
-			print(member.value, ":", name) 
+			print(member.value, ":", name)
+
+	# Gets the user's input on which player to choose
+	def get_player():		
+		# Loop untl we get appropriate input
+		choice = 0
+		while True:
+			choice = int(input())
+
+			if choice > 0  and choice < 7:
+				return choice 
+			else:
+				print("Invalid player, please try again")
 
 
 
 # Program entry point
-# player_1 = Rock(Elem.rock)
-# player_1 = Paper(Elem.paper)
-# player_1 = Scissors(Elem.scissors)
-# player_1 = Spock(Elem.spock)
-# player_1 = Lizard(Elem.lizard)
-# player_2 = Rock(Elem.rock)
-# player_2 = Paper(Elem.paper)
-# player_2 = Scissors(Elem.scissors)
-# player_2 = Spock(Elem.spock)
-# player_2 = Lizard(Elem.lizard)
-# print(player_1.compareTo(player_2))
-
-main_instance = Main.main()
+Main.main()
 
 
 
